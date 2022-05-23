8a821dffb05c3467a3a7d8f0b4ac697247191245
Konul304 <memmedovakonul270@gmail.com>
2022-05-23 22:36:01 +0400

    first commit

---
+++ movie-app/src/components/MoviesHeader.js
@@ -0,0 +1,9 @@
+const MoviesHeader = (props) => {
+	return (
+		<div className='col'>
+			<h1>{props.heading}</h1>
+		</div>
+	);
+};
+
+export default MoviesHeader;
---
+++ movie-app/src/components/SearchBox.js
@@ -0,0 +1,15 @@
+
+const SearchBox = (props) => {
+	return (
+		<div className='col col-sm-4'>
+			<input
+				className='form-control'
+				value={props.value}
+				onChange={(event) => props.setSearchValue(event.target.value)}
+				placeholder='Search by title...'
+			></input>
+		</div>
+	);
+};
+
+export default SearchBox;
